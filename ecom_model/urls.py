cb32142177622b3749c1595f9b49d9a2c50ce06b
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-06 03:55:48 -0500

    updated urls on settings

--- ecom_model/urls.py
+++ ecom_model/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     # Cart app
     path('cart/', include('cart.urls')),
     # Account app
-    #path('account/', include('account.urls')),
+    path('account/', include('account.urls')),
     # Payment app
     #path('payment/', include('payment.urls')),
 ]
