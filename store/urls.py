eba27c95dead8171bedc8238c737c65f01785ee3
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-08 12:55:33 -0500

    modified base, urls, and views and added the search feature

--- store/urls.py
+++ store/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('product/<slug:product_slug>/', views.product_info, name='product-info'),
     # Individual category
     path('search/<slug:category_slug>/', views.list_category, name='list-category'),
+    # Search products
+    path('search-products/', views.search_products, name='search-products'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from . models import Category, Product
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+
 
 def store(request):
     all_products = Product.objects.all()
@@ -20,3 +24,27 @@ def product_info(request, product_slug):
     product = get_object_or_404(Product, slug=product_slug)
     context = {'product': product}
     return render(request, 'store/product-info.html', context)
+
+def search_products(request):
+    query = request.GET.get('q', '')
+    is_ajax = request.GET.get('ajax', '') == '1'
+    if query:
+        # Search in product title, brand, and description
+        products = Product.objects.filter(
+            Q(title__icontains=query) | 
+            Q(brand__icontains=query) | 
+            Q(description__icontains=query)
+        ).distinct()[:10]  # Limit to 10 results for AJAX
+    else:
+        products = Product.objects.none()
+    # If this is an AJAX request, return HTML snippet for suggestions
+    if is_ajax:
+        html = render_to_string('store/search-suggestions.html', {'products': products, 'query': query})
+        return HttpResponse(html)
+    # Otherwise, return full page
+    context = {
+        'products': products,
+        'query': query,
+        'product_count': products.count()
+    }
+    return render(request, 'store/search-results.html', context)
