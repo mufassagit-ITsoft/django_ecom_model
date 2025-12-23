407a3c9812c9e6937becc187f24b5453fa2c975d
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-23 10:23:07 -0500

    modified views on payment

--- payment/views.py
+++ payment/views.py
@@ -1,70 +1,39 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from . models import ShippingAddress, Order, OrderItem
-
 from cart.cart import Cart
-
 from django.http import JsonResponse
-
 from django.core.mail import send_mail
-
 from django.conf import settings
 
 
 def checkout(request):
-
     # Users with accounts -- Pre-fill the form
-
     if request.user.is_authenticated:
-
         try:
-
             # Authenticated users WITH shipping information 
-
             shipping_address = ShippingAddress.objects.get(user=request.user.id)
-
             context = {'shipping': shipping_address}
-
-            
-
-
             return render(request, 'payment/checkout.html', context=context)
-
-
         except:
-
             # Authenticated users with NO shipping information
-
             return render(request, 'payment/checkout.html')
-
     else:
-            
         # Guest users
-
         return render(request, 'payment/checkout.html')
 
-
-
 def complete_order(request):
-
     if request.POST.get('action') == 'post':
-
         name = request.POST.get('name')
         email = request.POST.get('email')
-
         address1 = request.POST.get('address1')
         address2 = request.POST.get('address2')
         city = request.POST.get('city')
-
         state = request.POST.get('state')
         zipcode = request.POST.get('zipcode')
 
         # All-in-one shipping address
         shipping_address = (address1 + "\n" + address2 + "\n" +
         city + "\n" + state + "\n" + zipcode)
-
         # Shopping cart information 
         cart = Cart(request)
 
@@ -136,24 +105,13 @@ def complete_order(request):
 
 
 def payment_success(request):
-
-
     # Clear shopping cart
-
-
     for key in list(request.session.keys()):
-
         if key == 'session_key':
-
             del request.session[key]
-
-
-
     return render(request, 'payment/payment-success.html')
 
-
 def payment_failed(request):
-
     return render(request, 'payment/payment-failed.html')
 
 
